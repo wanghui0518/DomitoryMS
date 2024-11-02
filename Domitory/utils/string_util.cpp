6c0666385dc0d2a55b0b6d1deacddf83ccdb546b
wanghui <wangh00518@gmail.com>
2024-11-02 14:19:59 +0800

    Update string_util.cpp
    add to_upper_copy function implementation

--- Domitory/utils/string_util.cpp
+++ Domitory/utils/string_util.cpp
@@ -38,8 +38,16 @@ std::string& string_util::reverse(std::string &str)
 	return str;
 }
 
+
 std::string& string_util::to_upper(std::string &str)
 {
 	transform(str.begin(), str.end(), str.begin(), ::toupper);
 	return str;
+}
+
+std::string string_util::to_upper_copy(std::string &str)
+{
+	std::string string_str_ret = str;
+	transform(string_str_ret.begin(), string_str_ret.end(), string_str_ret.begin(), ::toupper);
+	return string_str_ret;
 }
