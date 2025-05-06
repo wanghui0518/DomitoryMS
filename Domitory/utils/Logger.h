87244a7fd865486bf5de14faa6958ff96278a34c
wanghui <wangh00518@gmail.com>
2025-05-06 17:08:27 +0800

    Update Logger.h

---
+++ Domitory/utils/Logger.h
@@ -0,0 +1,14 @@
+#ifndef _LOGGER_H_
+#define _LOGGER_H_
+
+// C++ Header File(s)
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+
+
+
+
+#endif // End of _LOGGER_H_
