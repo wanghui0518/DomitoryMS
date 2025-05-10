e8e856e605e57d8f5395d01717d1d9667491d19a
wanghui <wangh00518@gmail.com>
2025-05-10 16:54:18 +0800

    Update Logger.h

--- Domitory/utils/Logger.h
+++ Domitory/utils/Logger.h
@@ -1,12 +1,22 @@
 #ifndef _LOGGER_H_
 #define _LOGGER_H_
 
+
 // C++ Header File(s)
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <string>
 
+#ifdef WIN32
+// Win Socket Header File(s)
+#include <Windows.h>
+#include <process.h>
+#else
+// POSIX Socket Header File(s)
+#include <errno.h>
+#include <pthread.h>
+#endif
 
 
 
