040d0711f8142b7f2f14261fff5e915146960dcd
wanghui <wangh00518@gmail.com>
2025-05-20 21:03:45 +0800

    Update Logger.h

--- Domitory/utils/Logger.h
+++ Domitory/utils/Logger.h
@@ -19,6 +19,18 @@
 #endif
 
 
+namespace CPlusPlusLogging
+{
+
+   #define LOG_ERROR(x)    Logger::getInstance()->error(x)
+   #define LOG_ALARM(x)	   Logger::getInstance()->alarm(x)
+   #define LOG_ALWAYS(x)	Logger::getInstance()->always(x)
+   #define LOG_INFO(x)     Logger::getInstance()->info(x)
+   #define LOG_BUFFER(x)   Logger::getInstance()->buffer(x)
+   #define LOG_TRACE(x)    Logger::getInstance()->trace(x)
+   #define LOG_DEBUG(x)    Logger::getInstance()->debug(x)
+
+}
 
 
 #endif // End of _LOGGER_H_
